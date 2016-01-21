b5a23eafdc69f48b809023c7c7d6307cfae7cb6e
TANIGUCHI Masaya <ta2gch@gmail.com>
2016-01-21 16:22:55 +0900

    add more tests

--- bin/test.sh
+++ bin/test.sh
@@ -1,9 +1,22 @@
 #! /bin/sh
 
+set -x
+
 PKGHOME="${PWD}/test"
 PKGBREW="${PWD}/bin/pkgbrew"
 
 . ./bin/installer > installer.log
 tail installer.log
+
+export PATH="${PKGHOME}/bin:${PATH}"
+
+pkgbrew install misc/less
+pkgbrew deinstall misc/less
+pkgbrew clean-deps misc/less
+pkgbrew clean misc/less
+pkgbrew tap NetBSD/pkgsrc-wip > /dev/null
+pkgbrew untap NetBSD/pkgsrc-wip > /dev/null
+pkgbrew test misc/less
+pkgbrew search emacs
 
 rm -rf "${PKGHOME}" installer.log
