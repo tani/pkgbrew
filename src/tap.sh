6a7b2e868f13e8718b6eabcb490144d489d7ccd1
asciian <asciian@outlook.jp>
2016-01-24 09:10:45 +0900

    remove an escape character

--- src/tap.sh
+++ src/tap.sh
@@ -18,7 +18,7 @@ tap_without_check(){
     download "https://github.com/${1}/archive/master.tar.gz" "-" \
 	| tar xz \
 	      --strip-components 1 \
-	      --directory "${PKGSRC}/`convert_repository_name "${1}"`" \
+	      --directory "${PKGSRC}/`convert_repository_name "${1}"`"
 }
 
 untap(){
