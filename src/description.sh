0853a2fa9b6389f67c3738128b39fd01429630c4
TANIGUCHI Masaya <masaya@ta2gch-pc>
2016-01-30 16:09:59 +0900

    disable a gnu extension

--- src/description.sh
+++ src/description.sh
@@ -3,7 +3,7 @@ description(){
     package="${PKGSRC}/${1}"
     if [ ! -n "${1}" -o ! -d "${package}" ]; then
 	echo No package \'${2}\' found, did you mead:
-	search "${1}" | sed --expression="s/^/ /"
+	search "${1}" | sed -e "s/^/ /"
 	echo pkgbrew: package not found
 	exit 1;
     fi
