03bcf395a268bfdb6c2089d1b421a08b5184ca6f
asciian <asciian@outlook.jp>
2016-02-03 16:27:52 +0900

    Change naming rule of directory

--- src/search.sh
+++ src/search.sh
@@ -1,6 +1,6 @@
 #?include src/config.sh
 search(){
-    packagedir=`dirname "${1}" | tr '/' '#'`
+    packagedir=`dirname "${1}" | tr '/' '_'`
     packagename=`basename "${1}"`
     find "${PKGSRC}" -maxdepth 2 -mindepth 2 -type d \
 	| grep "${packagedir}/${packagename}" \
